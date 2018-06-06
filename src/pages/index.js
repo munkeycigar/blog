49652f03fc60daf5569a0254c153e8c126208c73
Leo <leoreyes1001@gmail.com>
2018-06-06 11:47:41 -0500

    removed netlify redirect after login

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,7 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <script>
-        if (window.netlifyIdentity) {
-            window.netlifyIdentity.on("init", user => {
-            if (!user) {
-                window.netlifyIdentity.on("login", () => {
-                document.location.href = "/admin/";
-                });
-            }
-            })
-        }
-    </script>
+    <h1>Leo Reyes</h1>
   </div>
 )
 
